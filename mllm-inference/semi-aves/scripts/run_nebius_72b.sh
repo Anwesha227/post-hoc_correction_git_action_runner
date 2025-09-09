9fbd39b00c81c28997ff15ca6777f22e3e94cd73
Anwesha227 <anwesha042@gmail.com>
2025-09-09 15:48:08 -0500

    Added grouped prompt

--- mllm-inference/semi-aves/scripts/run_nebius_72b.sh
+++ mllm-inference/semi-aves/scripts/run_nebius_72b.sh
@@ -11,14 +11,14 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 BASE_MODEL="vitb32_openclip_laion400m"   # or openclip, resnet, ...
 
 # Output + error paths that include BASE_MODEL
-OUTPUT_CSV="${SCRIPT_DIR}/../mllm_output/nebius_${BASE_MODEL}_Qwen_Qwen2.5-VL-72B-Instruct_qwen_top5_flat_taxonomy_withconf.csv"
-ERROR_LOG="${SCRIPT_DIR}/../mllm_output/nebius_${BASE_MODEL}_Qwen_Qwen2.5-VL-72B-Instruct_qwen_top5_flat_taxonomy_withconf_errors.txt"
+OUTPUT_CSV="${SCRIPT_DIR}/../mllm_output/nebius_${BASE_MODEL}_Qwen_Qwen2.5-VL-72B-Instruct_qwen_top5_grouped.csv"
+ERROR_LOG="${SCRIPT_DIR}/../mllm_output/nebius_${BASE_MODEL}_Qwen_Qwen2.5-VL-72B-Instruct_qwen_top5_grouped_errors.txt"
 
 TOPK_JSON="${SCRIPT_DIR}/../../../data/semi-aves/topk/fewshot_finetune_vitb32_openclip_laion400m_semi-aves_16_1_topk_test_predictions.json"
 
 "${SCRIPT_DIR}/run_qwen.sh" \
   --backend nebius \
-  --python "${SCRIPT_DIR}/../qwen_top5_flat_taxonomy_withconf.py" \
+  --python "${SCRIPT_DIR}/../qwen_top5_grouped.py" \
   --image-dir "${SCRIPT_DIR}/../../../datasets/semi-aves" \
   --image-paths "${SCRIPT_DIR}/../../../data/semi-aves/test.txt" \
   --topk-json "${TOPK_JSON}" \
